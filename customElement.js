684eeb0982faaade0b72a870aa98475dc55c3e4e
Diodr <137680166+Diodr@users.noreply.github.com>
2023-06-25 12:45:27 +0500

    Add files via upload

---
+++ customElement.js
@@ -0,0 +1,56 @@
+loadsModules["customElement"]=true
+
+
+// class NEW_ELEMENT extends HTMLElement {
+
+
+
+
+//   disconnectedCallback() {
+//     // браузер вызывает этот метод при удалении элемента из документа
+//     // (может вызываться много раз, если элемент многократно добавляется/удаляется)
+//   }
+
+
+
+//   attributeChangedCallback(name, oldValue, newValue) {
+//     // вызывается при изменении одного из перечисленных выше атрибутов
+//   }
+
+//   adoptedCallback() {
+//     // вызывается, когда элемент перемещается в новый документ
+//     // (происходит в document.adoptNode, используется очень редко)
+//   }
+
+
+// }
+
+// customElements.define("no-my-new-element", NEW_ELEMENT);
+
+class MyElements {
+  static createNewElement(NameElement,html,attributs=[],...args){
+    
+    eval(`class ELEM extends HTMLElement {
+      allAtr = ${JSON.stringify(attributs)}
+      inhtml = '${html}'
+
+      connectedCallback() {
+        this.innerHTML = this.inhtml
+      }
+      static get observedAttributes() {
+        return allAtributes();
+      }
+
+      allAtributes(){
+        return this.allAtr
+      }
+    }
+    customElements.define((NameElement.search("-")!=-1?NameElement :("my-"+NameElement)),ELEM)
+    
+    `)
+    
+  }
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,96 @@
+var head = document.head
+head.innerHTML += `<style>
+import, load, endloader{
+    display:none;
+}
+window{
+    display:block;
+    padding:0px;
+    margin:0px;
+    position:relative;
+}
+body,html{
+    padding:0px;
+    margin:0px;
+}
+</style>`
+
+var loadsModules = {
+
+}
+
+
+var PAGES = {
+    
+}
+
+
+var endloader = document.getElementsByTagName('endloader')[0]
+
+var miniVars = {}
+
+var WINDOW 
+
+
+window.onload = () => {
+    
+    document.getElementsByTagName('body')[0].innerHTML += `<window></window>`
+    WINDOW = document.getElementsByTagName('window')[0]
+    
+    let loads = document.getElementsByTagName('load')
+    
+    endloader = document.getElementsByTagName('endloader')[0]
+    
+    let jss = []
+
+    for (var i = 0; i < loads.length; i++) {
+        let load = loads[i]
+        for (let el = 0; el < load.children.length; el++) {
+            let element = load.children[el]
+            
+            console.log(element);
+            switch (element.tagName) {
+                case "PAGE":
+                    PAGES[element.attributes.name.value] = element.attributes.src.value
+                    break;
+                
+                case "MODULE":
+                    loadsModules[element.attributes.name.value]=false
+                    IMPORT("module/"+element.attributes.name.value+".js")
+                    break;
+
+                case "JS":
+                    jss[jss.length]=element.attributes.src.value
+                    break
+                
+                default:
+                    break;
+            }
+            
+        }
+        
+        
+        
+        //endloader.appendChild(resource)
+        
+    }
+    
+    var loadsScripts = setInterval((jss,lm)=>{
+        if (Object.values(lm).indexOf(false) == -1) {
+            for (let i = 0; i < jss.length; i++) {
+                const js = jss[i];
+                IMPORT(js)
+            }
+            clearInterval(loadsScripts)
+
+        }
+
+    },500,jss,loadsModules)
+}
+
+
+function IMPORT(file) {
+    let sc = document.createElement('script')
+    sc.src = file
+    endloader.appendChild(sc)
+}
